afd23692ad3a3b9479c7da5047fc2310a8cc6f4f
monar-22 <141346716+monar-22@users.noreply.github.com>
2024-02-15 14:26:50 +0530

    Time: 13 ms (46.36%), Space: 45.6 MB (22.61%) - LeetHub

---
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] distinctDifferenceArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> hs = new HashSet<>();
+            for (int j = 0; j <= i; j++) {
+                hs.add(nums[j]);
+            }
+            HashSet<Integer> js = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                js.add(nums[j]);
+            }
+            res[i] = hs.size()-js.size();
+        }
+        return res;
+    }
+}
