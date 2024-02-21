94f90e1f4bf2b17fd64561707fae7278a476e6d6
monar-22 <141346716+monar-22@users.noreply.github.com>
2024-02-21 06:21:37 +0530

    Time: 760 ms (55.65%), Space: 0B (100.00%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT DISTINCT v1.author_id AS id
+FROM Views v1
+JOIN Views v2 ON v1.article_id = v2.article_id
+             AND v1.author_id = v2.viewer_id
+             AND v1.viewer_id = v2.author_id
+ORDER BY id;
